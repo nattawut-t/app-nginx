b8a73bb141dc63c248bb3da1e138e5881b3be4b7
j2sdk_ball2 <nattawut@moneytable.com>
2017-12-02 09:09:36 +0700

    modify local script: 1. run webpack build app 2. copy dist/ to /var/www/app/

--- local.sh
+++ local.sh
@@ -1,5 +1,14 @@
 # sudo -s "echo '127.0.0.1       nattawut.io app.nattawut.io' >> /etc/hosts"
+npm --prefix ../url-shortener-app/ run build
+# 
+rm -rf var/www/app
+cp -r ../url-shortener-app/dist/ var/www/app
+# 
 docker rm -f app-nginx
 docker rmi app-nginx:v1.0.0
 docker build -t app-nginx:v1.0.0 .
-docker run --name=app-nginx -p 80:80 -p 443:443 -d app-nginx:v1.0.0
+docker run \
+        --name=app-nginx \
+        -p 80:80 \
+        -p 443:443 \
+        -d app-nginx:v1.0.0
